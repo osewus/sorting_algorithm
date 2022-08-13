24aa4a9c79f99d98950d5a4d05891ffdcb5e415b
osewus <34625293+osewus@users.noreply.github.com>
2022-08-13 01:26:17 +0000

    Update 103-merge_sort.c

--- 103-merge_sort.c
+++ 103-merge_sort.c
@@ -1,79 +1,145 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "sort.h"
+
 /**
- * prints - prints left, right, and merged halves
- * @arr: original or tmp array
- * @side: left, right, or merged half
- * @start: starting index
- * @end: ending index
+ * copy - copies data from one buffer to another
+ *
+ * @src: source buffer
+ * @dst: destination buffer
+ * @size: size of buffers
+ *
+ * Return: No Return
  */
-void prints(int *arr, char *side, size_t start, size_t end)
+void copy(int *src, int *dst, int size)
 {
-	size_t i;
+	int i;
 
-	printf("[%s]: ", side);
-	for (i = start; i < end; i++)
-	{
-		if (i != (end - 1))
-			printf("%d, ", arr[i]);
-		else
-			printf("%d\n", arr[i]);
-	}
-
+	for (i = 0; i < size; i++)
+		dst[i] = src[i];
 }
 /**
- * rec_merge - recursively splits and merges halves
- * @array: original array
- * @sortArr: tmp array to hold sorted elements
- * @l: starting index
- * @r: ending index
+ * merge - merges two sets of data in ascending order
+ * but they must already be sorted before hand
+ * @array: first set of data
+ * @buff: second set of data
+ * @minL: lower range of first set of data
+ * @maxL: upper range of first set of data
+ * @minR: lower range of second set of data
+ * @maxR: upper range of second set of data
+ *
+ * Return: No Return
  */
-void rec_merge(int *array, int *sortArr, size_t l, size_t r)
+void merge(int *array, int *buff, int minL, int maxL, int minR, int maxR)
 {
-	size_t i, l_half, r_half;
-	size_t mid = (l + r) / 2;
+	int i = minL, j = minR, k = minL;
 
-	if (r - l <= 1)
+	while (i <= maxL || j <= maxR)
+
+		if (i <= maxL && j <= maxR)
+			if (buff[i] <= buff[j])
+				array[k] = buff[i], k++, i++;
+			else
+				array[k] = buff[j], k++, j++;
+
+		else if (i > maxL && j <= maxR)
+			array[k] = buff[j], k++, j++;
+		else
+			array[k] = buff[i], k++, i++;
+}
+/**
+ * printcheck - prints an array in a given range
+ *
+ * @array: array of data to be print
+ * @r1: start of range
+ * @r2: end of range
+ *
+ * Return: No Return
+ */
+void printcheck(int *array, int r1, int r2)
+{
+	int i;
+
+	for (i = r1; i <= r2; i++)
+	{
+		if (i > r1)
+			printf(", ");
+		printf("%d", array[i]);
+	}
+	printf("\n");
+}
+/**
+ * split - recursive function to split data into merge tree
+ *
+ * @array: array of data to be split
+ * @buff: auxiliary array of data for merging
+ * @min: min range of data in array
+ * @max: max range of data in array
+ * @size: size of total data
+ *
+ * Return: No Return
+ */
+void split(int *array, int *buff, int min, int max, int size)
+{
+	int mid, tmax, minL, maxL, minR, maxR;
+
+	if ((max - min) <= 0)
 		return;
 
-	rec_merge(array, sortArr, l, mid);
-	rec_merge(array, sortArr, mid, r);
+	mid = (max + min + 1) / 2;
+	tmax = max;
+	max = mid - 1;
+
+	minL = min;
+	maxL = max;
+
+	split(array, buff, min, max, size);
+
+	min = mid;
+	max = tmax;
+
+	minR = min;
+	maxR = max;
+
+	split(array, buff, min, max, size);
+
 	printf("Merging...\n");
-	prints(array, "left", l, mid);
-	prints(array, "right", mid, r);
-	l_half = l;
-	r_half = mid;
-	for (i = l; i < r; i++)
-	{
-		if ((l_half < mid) &&
-		    ((r_half == r) || (array[l_half] < array[r_half])))
-		{
-			sortArr[i] = array[l_half];
-			l_half++;
-		}
-		else
-		{
-			sortArr[i] = array[r_half];
-			r_half++;
-		}
-	}
-	prints(sortArr, "Done", l, r);
-	for (i = l; i < r; i++)
-		array[i] = sortArr[i];
+	printf("[left]: ");
+
+	printcheck(array, minL, maxL);
+
+	printf("[right]: ");
+
+	printcheck(array, minR, maxR);
+	merge(array, buff, minL, maxL, minR, maxR);
+	copy(array, buff, size);
+
+	printf("[Done]: ");
+	printcheck(array, minL, maxR);
 }
 /**
- * merge_sort - sorts mergly
- * @array: array sort
- * @size: size of array
+ * merge_sort - sorts an array of integers in ascending order
+ * using the Merge sort algorithm
+ *
+ * @array: array of data to be sorted
+ * @size: size of data
+ *
+ * Return: No Return
  */
 void merge_sort(int *array, size_t size)
 {
-	int *sortArr;
+	int *buff;
 
-	if (!(array) || size < 2)
+	if (size < 2)
 		return;
-	sortArr = malloc(sizeof(int) * size);
-	if (!(sortArr))
+
+	buff = malloc(sizeof(int) * size);
+	if (buff == NULL)
 		return;
-	rec_merge(array, sortArr, 0, size);
-	free(sortArr);
+
+	copy(array, buff, size);
+
+	split(array, buff, 0, size - 1, size);
+
+	free(buff);
 }
